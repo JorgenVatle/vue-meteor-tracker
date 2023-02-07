03173307a88315e6e07ee445c3680b926370c3bc
Jørgen Vatle <Jorgen@WcAServices.net>
2023-02-08 00:50:30 +0100

    Remove unnecessary indent from beforeCreate() hook.

--- src/index.ts
+++ src/index.ts
@@ -202,36 +202,38 @@ export const VueMeteor = {
   install (app: App) {
     app.mixin({
       beforeCreate () {
-        if (this.$options.meteor) {
-          const subReady = reactive<Record<string, boolean>>({})
+        if (!this.$options.meteor) {
+          return
+        }
 
-          if (this.$options.meteor.$subscribe) {
-            for (const key in this.$options.meteor.$subscribe) {
-              const value = this.$options.meteor.$subscribe[key]
-              const { ready } = typeof value === 'function'
-                ? subscribe(() => {
-                  const result = value.call(this)
-                  return [key, ...result]
-                })
-                : subscribe(key, ...value)
-              // @ts-expect-error unwrapping
-              subReady[key] = ready
-            }
+        const subReady = reactive<Record<string, boolean>>({})
+
+        if (this.$options.meteor.$subscribe) {
+          for (const key in this.$options.meteor.$subscribe) {
+            const value = this.$options.meteor.$subscribe[key]
+            const { ready } = typeof value === 'function'
+              ? subscribe(() => {
+                const result = value.call(this)
+                return [key, ...result]
+              })
+              : subscribe(key, ...value)
+            // @ts-expect-error unwrapping
+            subReady[key] = ready
           }
+        }
 
-          this.$options.computed = this.$options.computed || {}
-          this.$options.computed.$subReady = () => subReady
+        this.$options.computed = this.$options.computed || {}
+        this.$options.computed.$subReady = () => subReady
 
-          const { subscribe: $subscribe } = useSubscribe()
-          this.$options.methods = this.$options.methods || {}
-          this.$options.methods.$subscribe = $subscribe
+        const { subscribe: $subscribe } = useSubscribe()
+        this.$options.methods = this.$options.methods || {}
+        this.$options.methods.$subscribe = $subscribe
 
-          for (const key in this.$options.meteor) {
-            if (key.startsWith('$')) continue
-            const fn = this.$options.meteor[key]
-            const { result } = autorun(fn.bind(this))
-            this.$options.computed[key] = () => result.value
-          }
+        for (const key in this.$options.meteor) {
+          if (key.startsWith('$')) continue
+          const fn = this.$options.meteor[key]
+          const { result } = autorun(fn.bind(this))
+          this.$options.computed[key] = () => result.value
         }
       },
     })
